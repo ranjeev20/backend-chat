17aa4677ba076e09acefc397b02da1383171b38f
ranjeev20 <ranjeevbohara2058@gmail.com>
2023-01-17 00:33:42 +0530

    added mongo uri

--- config/db.js
+++ config/db.js
@@ -3,7 +3,7 @@ const colors = require("colors");
 
 const connectDB = async () => {
   try {
-    const conn = await mongoose.connect(MONGO_URI, {
+    const conn = await mongoose.connect(process.env.MONGO_URI, {
       useNewUrlParser: true,
       useUnifiedTopology: true,
       useFindAndModify: true,
